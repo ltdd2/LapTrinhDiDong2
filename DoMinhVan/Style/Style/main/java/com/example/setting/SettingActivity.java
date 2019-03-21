c5d9d5173aa314e72e8efd9cc9228d40f8a2ed8c
DoMinhVan <43976823+DoMinhVan@users.noreply.github.com>
2019-03-21 19:50:19 +0700

    xóa

---
+++ DoMinhVan/Style/Style/main/java/com/example/setting/SettingActivity.java
@@ -0,0 +1,13 @@
+package com.example.setting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setting_layout);
+    }
+}
